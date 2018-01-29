52e0dae079844a74899cdbf092c406013b0ecfd6
meilijia <1056253401@qq.com>
2018-01-29 11:17:15 +0800

    change

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <index-footer></index-footer>
+  </div>
+</template>
+
+<script>
+import IndexFooter from './components/footer'
+export default {
+  name: 'App',
+  components: {
+    IndexFooter
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer">footer</div>
+</template>
+
+<script>
+export default {
+  name: 'index-footer'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .footer
+    position: fixed
+    bottom: 0
+    height: 1.2rem
+    width: 100%
+    background: #000
+    color: #fff
+</style>
